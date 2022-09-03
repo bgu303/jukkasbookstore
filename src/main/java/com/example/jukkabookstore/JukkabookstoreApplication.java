cfe76f990646d289912417666d845c5db95a71e5
bgu303 <bgu303@myy.haaga.helia.fi>
2022-09-03 20:27:46 +0300

    modified:   src/main/java/com/example/jukkabookstore/JukkabookstoreApplication.java

--- src/main/java/com/example/jukkabookstore/JukkabookstoreApplication.java
+++ src/main/java/com/example/jukkabookstore/JukkabookstoreApplication.java
@@ -3,7 +3,7 @@ package com.example.jukkabookstore;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-// moi
+// moi muutos
 
 
 @SpringBootApplication
